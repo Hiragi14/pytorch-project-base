cad9cb3d428e472566bf66bca67439c5bc6c488d
kawai <kawai.syusei@gmail.com>
2025-02-05 03:14:36 +0900

    mlp model add

---
+++ src/pytorch-project-base/models/mlp.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+class MLP(nn.Module):
+    def __init__(self, layer_sizes):
+        super(MLP, self).__init__()
+        layers = []
+        for i in range(len(layer_sizes) - 1):
+            layers.append(nn.Linear(layer_sizes[i], layer_sizes[i+1]))
+        self.layers = nn.ModuleList(layers)
+        self.activations = []
+        self.z_values = []
+        
+        self.flatten = nn.Flatten()
+        
+    def forward(self, x):
+        x = self.flatten(x)
+        self.activations = [x]
+        self.z_values = []
+        for layer in self.layers[:-1]:
+            x = layer(x)
+            self.z_values.append(x)
+            x = F.relu(x)
+            self.activations.append(x)
+        x = self.layers[-1](x)
+        self.out = x
+        return x
