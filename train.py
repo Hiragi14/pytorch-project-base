b86e16a4ca8b8b24b90fc80bc580d0583705ab00
Hiragi14 <kawai.syusei.s0@ist.hokudai.ac.jp>
2025-02-05 15:58:00 +0900

    追加

---
+++ train.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append('/DeepLearning/Users/kawai/Document/pytorch-project-base/')
+sys.path.append('/DeepLearning/Users/kawai/Document/pytorch-project-base/src/pytorch-project-base/')
+import os
+import logging
+import argparse
+import json
+import src as src
+from src import *
+from main import main
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    logger.info("Hello, world!")
+
+    parser = argparse.ArgumentParser(description='PyTorch Training')
+    parser.add_argument('--config', default='config.json', type=str, help='config file path (default: config.json)')
+    args = parser.parse_args()
+
+    config = json.load(open(args.config))
+    main(config)
