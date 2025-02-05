005c1fbe534461814ccc9f046afc9a55385b067a
Hiragi14 <kawai.syusei.s0@ist.hokudai.ac.jp>
2025-02-05 15:56:45 +0900

    無限呼び出しを修正

--- src/pytorch-project-base/dataloader/dataloaders.py
+++ src/pytorch-project-base/dataloader/dataloaders.py
@@ -6,14 +6,14 @@ class MnistDataLoader(BaseDataLoader):
     """
     MNIST data loading demo using BaseDataLoader
     """
-    def __init__(self, data_dir, batch_size, shuffle, validation_split, num_workers):
+    def __init__(self, data_dir, batch_size, shuffle, num_workers, train=True, validation_split=0.0, download=False):
         trsfm = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.1307,), (0.3081,))
         ])
         self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=True, download=False, transform=trsfm)
+        self.dataset = datasets.MNIST(self.data_dir, train=train, download=download, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, num_workers, validation_split)
         
-    def get_val_loader(self):
-        return self.get_loader(sampler=self.val_sampler)
+    # def get_val_loader(self):
+    #     return self.get_val_loader()
